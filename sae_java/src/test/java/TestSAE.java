192c514fb75ea367ab3c09149ba444ad39abd0e6
Revan <quentin.chaufour@etu.univ-orleans.fr>
2025-05-01 16:07:40 +0200

    test : tests for Panier, panier in Client and Books

--- sae_java/src/test/java/TestSAE.java
+++ sae_java/src/test/java/TestSAE.java
@@ -1,6 +1,13 @@
 
+import java.lang.reflect.Array;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TestSAE {
     
@@ -17,6 +24,7 @@ public class TestSAE {
         assertEquals("45000", client.getCodePostal());
         assertEquals("Orléans", client.getVille());
         assertEquals(new Librairie(7,"Loire et livres", "Orléans"), client.getLibrairie());
+        assertEquals(new Panier(), client.getPanier());
         
         client.setLibrairie(new Librairie(1,"La librairie parisienne", "Paris"));
         client.setNom("Dupont");
@@ -32,6 +40,7 @@ public class TestSAE {
         assertEquals("Paris", client.getVille());
         assertEquals(new Librairie(1,"La librairie parisienne", "Paris"), client.getLibrairie());
         // pas de setteur pour idClient car c'est un attribut non modifiable (final)
+
     }
 
     @Test
@@ -50,6 +59,62 @@ public class TestSAE {
 
         assertEquals("La librairie de Lyon", librairie.getNom());
         assertEquals("Lyon", librairie.getVille());
+    }
+
+    @Test
+    public void testLivre() {
+
+        Livre livre = new Livre("120","La Guerre des mondes", Arrays.asList(new Auteur(1,"H.G. Wells")), "Gallimard", 1898,9.99, 159, "Science Fiction");
+
+        assetEquals("120", livre.getIsbn());
+        assertEquals("La Guerre des mondes", livre.getTitre());
+        assertTrue(livre.getAuteurs().contains(new Auteur(1,"H.G. Wells")));
+        assertEquals("Gallimard", livre.getEditeur());
+        assertEquals(1898, livre.getAnneePublication());
+        assertEquals(9.99, livre.getPrix(), 0.00);
+        assertEquals(159, livre.getNombrePages());
+        assertEquals("Science Fiction", livre.getClassification());
+
+        livre.setPrix(12.99);
+        
+        assertEquals(12.99, livre.getPrix(), 0.00);
+    }
+
+    @Test
+    public void testPanier() {
+
+        Panier panier = new Panier();
+        Livre livre1 = new Livre("120","La Guerre des mondes",  Arrays.asList(new Auteur(1,"H.G. Wells")), "Gallimard", 1898,9.99, 159, "Science Fiction");
+        Livre livre2 = new Livre("121","Le Petit Prince",  Arrays.asList(new Auteur(2,"Antoine de Saint-Exupéry")), "Gallimard", 1943, 7.99, 96, "Roman");
+
+        // Test ajout de livres
+        panier.ajouterLivre(livre1);
+        panier.ajouterLivre(livre2);
+
+        assertTrue(panier.getContenu().contains(livre1));
+        assertTrue(panier.getContenu().contains(livre2));
+
+        assertTrue(panier.getContenu().equals(Arrays.asList(livre1, livre2)));
+
+        // Test suppression de livres
+        panier.removeLivre(livre1);
+        assertFalse(panier.getContenu().contains(livre1));
+
+    }
+
+    @Test
+    public void testPanierClient(){
+
+        Client client = new Client("Julie", "Martin", 3, "133 boulevard de l''Université", "45000", "Orléans",new Librairie(7,"Loire et livres", "Orléans"));
+
+        // Test ajout/suppression de livres au panier du client
+        Livre livre = new Livre("120","La Guerre des mondes", Arrays.asList(new Auteur(1,"H.G. Wells")), "Gallimard", 1898,9.99, 159, "Science Fiction");
+        client.ajouterLivre(livre);
+        assertTrue(client.getPanier().getContenu().contains(livre));
+        assertFalse(client.getPanier().getContenu().contains(new Livre("121","Le Petit Prince", Arrays.asList(new Auteur(2,"Antoine de Saint-Exupéry")), "Gallimard", 1943, 7.99, 96, "Roman")));
+
+        client.removeLivre(livre);
+        assertFalse(client.getPanier().getContenu().contains(livre));
     }
 
 }
