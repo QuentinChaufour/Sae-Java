682a9249bfd8871616bad8bcbd82711d850ba2cc
JolanRolland <jolan.rolland@etu.univ-orleans.fr>
2025-06-17 14:44:08 +0200

    Maj du fichier Application pour prendre en compte la partie vendeur

--- sae_java/src/main/java/com/sae_java/Vue/VendeurWindow.java
+++ sae_java/src/main/java/com/sae_java/Vue/VendeurWindow.java
@@ -1,153 +1,173 @@
 package com.sae_java.Vue;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.TextField;
-import com.sae_java.Vue.controleur.*;
+import javafx.stage.Stage;
 
+import com.sae_java.Modele.Client;
 import com.sae_java.Modele.Vendeur;
-import com.sae_java.Modele.Reseau;
-import com.sae_java.Modele.Exceptions.LibraryNotFoundException;
-
-public class VendeurWindow extends BorderPane{
-
-    private final ApplicationSAE app;
-    private final Vendeur vendeur;
 
 
-    public VendeurWindow(ApplicationSAE app, Vendeur vendeur) {
-        this.app = app;
-        this.vendeur = vendeur;
-        
-        this.minHeightProperty().set(600);
-        this.minWidthProperty().set(1200);
+public class ApplicationSAE extends Application {
 
-        // top of the borderPane
+    private Stage stage;
+    private Scene scene;
 
-        BorderPane top = new BorderPane();
-        
+    private Client client;
+    private Vendeur vendeur;
 
-        // Create a button for "Deconnexion"
-        Button deconnexion = new Button("Deconnexion");
-        deconnexion.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/deconnexion_32px.png"))));
-        deconnexion.setOnAction(new ControleurDeconnexion(this.app));
+    private Connection connection;
+    //private ImageView imageView;
 
-        // create a button for Panier 
-
-
-        BorderPane.setMargin(top, new Insets(0, 0, 10, 0));
-        BorderPane.setMargin(deconnexion, new Insets(5));
-        BorderPane.setAlignment(deconnexion, Pos.CENTER_LEFT);
-
-
-        top.setLeft(deconnexion);
-
-        this.setTop(top);
-
-        // left side of the borderPane
-
-        // client infos
-
-        HBox vendeurInfo = new HBox(10);
-        vendeurInfo.setStyle("-fx-padding: 10; -fx-background-color: #d7fffb; -fx-border-color: #000000; -fx-border-width: 2;");
-        Label vendeurName = new Label(app.getVendeur().getNom());
-        Label vendeurForname = new Label(app.getVendeur().getPrenom());
-        ImageView vendeurImage = new ImageView(new Image(getClass().getResourceAsStream("/images/utilisateur.png")));
-
-        vendeurImage.setFitHeight(32);
-        vendeurImage.setFitWidth(32);
-        vendeurImage.setPreserveRatio(true);
-        vendeurImage.setSmooth(true);
-
-        BorderPane.setAlignment(vendeurImage, Pos.CENTER_LEFT);
-        vendeurName.setAlignment(Pos.CENTER);
-        vendeurForname.setAlignment(Pos.CENTER);
-
-        vendeurInfo.getChildren().addAll(vendeurImage, vendeurName, vendeurForname);
-
-        VBox left = new VBox(10);
-
-        // buttons
-
-        Label librairieLabel = new Label();
+    @Override
+    public void init(){
+    /*
+        // récupérer le plus grand numCom existant
         try {
-            librairieLabel.setText("Librairie : " + Reseau.getLibrairie(app.getVendeur().getLibrairie()));
-        } 
-        catch (LibraryNotFoundException e) {
-
-            // alert ?
-
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/testImage", "root", "root_password");
+            this.connection = connection;
+            // Récupère le plus grand numCom existant
+            
+        }catch (SQLException e) {
             e.printStackTrace();
-        }
-        // add image to the button for ergonomics
-
-        Button changerInfos = new Button("Update infos");
-        changerInfos.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/reglages_32px.png"))));
+           }
+     */   
 
 
-        left.getChildren().addAll(vendeurInfo,librairieLabel, changerInfos);
-        left.setStyle("-fx-margin: 10;-fx-padding: 10; -fx-background-color: #ffcae2;");
-        this.setLeft(left);
-
-        // center of the borderPane
-        VBox center = new VBox(10);
-        VBox addLivre = new VBox(5);
-        addLivre.setPadding(new Insets(15));
-
-        VBox insideAddLivre = new VBox(3);
-        insideAddLivre.setPadding(new Insets(10));
-        insideAddLivre.setStyle("-fx-background-color: #d7fffb;");
-
-        Label addLivreLabel = new Label("Ajouter un livre");
-
-        HBox addLivreInputBox = new HBox(10);
-
-        Label isbnLabel = new Label("ISBN :");
-        TextField isbnArea = new TextField();
-
-        Label quantityLabel = new Label("Quantité :");
-        TextField quantityArea = new TextField();
-
-        Button addButton = new Button("Ajouter");
-
-        addLivreInputBox.getChildren().addAll(isbnLabel, isbnArea, quantityLabel, quantityArea, addButton);
-        insideAddLivre.getChildren().addAll(addLivreLabel, addLivreInputBox);
-        addLivre.getChildren().add(insideAddLivre);
-
-
-
-        VBox updateQuantity = new VBox(5);
-        VBox insideUpdateQuantity = new VBox(3);
-        Label addUpdateQuantityLabel = new Label("Modifier quantité");
-        addLivre.getChildren().addAll(insideUpdateQuantity);
-        insideUpdateQuantity.getChildren().addAll(addUpdateQuantityLabel);
-
-
-        VBox checkAvailability = new VBox(5);
-
-        center.getChildren().addAll(addLivre, updateQuantity, checkAvailability);
-
-
-        // bottom of the borderPane
-        HBox bottom = new HBox(10);
-        Label credit = new Label("V1 par Jolan");
-        bottom.getChildren().add(credit);
-
-        this.setCenter(center);
-        this.setBottom(bottom);
+        this.client = new Client("Dupont", "Jean", "1234",1, "1 rue de la paix", "75000", "Paris", 1);
+        this.vendeur = new Vendeur("Dupont", "Jean", "1234",1, 1);
+        //this.imageView = new ImageView();
+        //this.imageView.setFitWidth(200);
+        //this.imageView.setFitHeight(200);
 
     }
 
-    public Vendeur getVendeur() {
+    @Override
+    public void start(Stage stage){
+
+        BorderPane root = new FenetreConnexion(this).getRoot();
+        /*
+        InputStream inputStream = getClass().getResourceAsStream("/images/quitter.png");
+        Image image = new Image(inputStream);
+        ImageView imageView = new ImageView(image);
+        */
+        
+        //BorderPane root = new BorderPane();
+
+        //Button filechooser = new Button("Choisir une image");
+        //filechooser.setOnAction(new ControleurFileChooser(this, this.stage));
+        
+        //root.setTop(filechooser);
+
+        this.scene = new Scene(root);
+        this.stage = stage;
+
+        this.stage.setScene(this.scene);
+        this.stage.setResizable(false);
+        this.stage.sizeToScene();
+        this.stage.setTitle("Mon application analyse");
+        this.stage.centerOnScreen();
+        this.stage.getIcons().add(new Image(getClass().getResource("/images/icon.png").toString()));
+        this.stage.show();
+
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void quitter(){
+        Platform.exit();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void inputDB(File monImage) {
+        try {
+
+            //File monImage = new File(getClass().getResource("/images/quitter.png").toString());
+            
+            FileInputStream fileInputStream = new FileInputStream(monImage);
+
+            PreparedStatement statement = this.connection.prepareStatement("INSERT INTO IMAGE values (?, ?)");
+
+            statement.setInt(1, 1);
+            statement.setBinaryStream(2, fileInputStream, (int) monImage.length());
+
+            statement.executeUpdate();
+            statement.close();
+            fileInputStream.close();
+
+        } catch (SQLException e) {
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayImage(int id){
+        try {
+            PreparedStatement statement = this.connection.prepareStatement("SELECT img FROM IMAGE WHERE id = 1");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            ImageView imageView = null;
+
+            if(resultSet.next()) {
+                byte[] imageBytes = resultSet.getBytes("img");
+                Image image = new Image(new ByteArrayInputStream(imageBytes));
+                imageView = new ImageView(image);
+            }
+
+            resultSet.close();
+            statement.close();
+            BorderPane root =  (BorderPane) this.stage.getScene().getRoot();
+            root.setCenter(imageView);
+
+            stage.sizeToScene();
+
+        } catch (SQLException e) {
+        }
+    }
+
+    public Client getClient() {
+        return this.client;
+    }
+
+     public Vendeur getVendeur() {
         return this.vendeur;
     }
 
+    public void loadIdentification(){
+
+        BorderPane root = new FenetreConnexion(this).getRoot();
+
+        this.scene = new Scene(root);
+
+        this.stage.setScene(this.scene);
+        this.stage.setResizable(false);
+        this.stage.sizeToScene();
+        this.stage.setTitle("Identification");
+        this.stage.centerOnScreen();
+        this.stage.getIcons().add(new Image(getClass().getResource("/images/icon.png").toString()));
+        this.stage.show();        
+    }
 }
