9e54142ef2154cb1ccca80848ba41e5115e61fb8
JolanRolland <jolan.rolland@etu.univ-orleans.fr>
2025-06-17 15:08:07 +0200

    Ajout des champs restants dans l'IHM de VendeurWindow

--- sae_java/src/main/java/com/sae_java/Vue/VendeurWindow.java
+++ sae_java/src/main/java/com/sae_java/Vue/VendeurWindow.java
@@ -22,129 +22,132 @@ public class VendeurWindow extends BorderPane{
     private final ApplicationSAE app;
     private final Vendeur vendeur;
 
-
     public VendeurWindow(ApplicationSAE app, Vendeur vendeur) {
         this.app = app;
         this.vendeur = vendeur;
-        
+
         this.minHeightProperty().set(600);
         this.minWidthProperty().set(1200);
-
-        // top of the borderPane
-
-        BorderPane top = new BorderPane();
         
-
-        // Create a button for "Deconnexion"
+        // TOP BAR
+        BorderPane top = new BorderPane();
         Button deconnexion = new Button("Deconnexion");
         deconnexion.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/deconnexion_32px.png"))));
         deconnexion.setOnAction(new ControleurDeconnexion(this.app));
-
-        // create a button for Panier 
-
-
-        BorderPane.setMargin(top, new Insets(0, 0, 10, 0));
         BorderPane.setMargin(deconnexion, new Insets(5));
         BorderPane.setAlignment(deconnexion, Pos.CENTER_LEFT);
-
-
         top.setLeft(deconnexion);
-
         this.setTop(top);
 
-        // left side of the borderPane
-
-        // client infos
-
+        // LEFT BAR - Info Vendeur
         HBox vendeurInfo = new HBox(10);
         vendeurInfo.setStyle("-fx-padding: 10; -fx-background-color: #d7fffb; -fx-border-color: #000000; -fx-border-width: 2;");
         Label vendeurName = new Label(app.getVendeur().getNom());
         Label vendeurForname = new Label(app.getVendeur().getPrenom());
         ImageView vendeurImage = new ImageView(new Image(getClass().getResourceAsStream("/images/utilisateur.png")));
-
         vendeurImage.setFitHeight(32);
         vendeurImage.setFitWidth(32);
-        vendeurImage.setPreserveRatio(true);
-        vendeurImage.setSmooth(true);
-
-        BorderPane.setAlignment(vendeurImage, Pos.CENTER_LEFT);
-        vendeurName.setAlignment(Pos.CENTER);
-        vendeurForname.setAlignment(Pos.CENTER);
-
         vendeurInfo.getChildren().addAll(vendeurImage, vendeurName, vendeurForname);
 
         VBox left = new VBox(10);
-
-        // buttons
-
         Label librairieLabel = new Label();
         try {
-            librairieLabel.setText("Librairie : " + Reseau.getLibrairie(app.getVendeur().getLibrairie()));
-        } 
-        catch (LibraryNotFoundException e) {
-
-            // alert ?
-
+            librairieLabel.setText("Librairie : " + Reseau.getLibrairie(app.getVendeur().getIdLibrairie()));
+        } catch (LibraryNotFoundException e) {
             e.printStackTrace();
         }
-        // add image to the button for ergonomics
-
         Button changerInfos = new Button("Update infos");
         changerInfos.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/reglages_32px.png"))));
-
-
-        left.getChildren().addAll(vendeurInfo,librairieLabel, changerInfos);
-        left.setStyle("-fx-margin: 10;-fx-padding: 10; -fx-background-color: #ffcae2;");
+        left.getChildren().addAll(vendeurInfo, librairieLabel, changerInfos);
+        left.setStyle("-fx-padding: 10; -fx-background-color: #ffcae2;");
         this.setLeft(left);
 
-        // center of the borderPane
-        VBox center = new VBox(10);
+        // CENTER - Actions
+        VBox center = new VBox(15);
+
+        // Ajouter un livre
         VBox addLivre = new VBox(5);
-        addLivre.setPadding(new Insets(15));
+        addLivre.setPadding(new Insets(10));
+        addLivre.setStyle("-fx-background-color: #d7fffb;");
+        Label addLivreLabel = new Label("Ajouter un livre au stock");
+        HBox addLivreInput = new HBox(10);
+        TextField addIsbn = new TextField();
+        addIsbn.setPromptText("ISBN");
+        TextField addQuantite = new TextField();
+        addQuantite.setPromptText("Quantité");
+        Button addLivreBtn = new Button("Ajouter");
+        addLivreInput.getChildren().addAll(new Label("ISBN :"), addIsbn, new Label("Quantité :"), addQuantite, addLivreBtn);
+        addLivre.getChildren().addAll(addLivreLabel, addLivreInput);
 
-        VBox insideAddLivre = new VBox(3);
-        insideAddLivre.setPadding(new Insets(10));
-        insideAddLivre.setStyle("-fx-background-color: #d7fffb;");
+        // Modifier quantité
+        VBox updateStock = new VBox(5);
+        updateStock.setPadding(new Insets(10));
+        updateStock.setStyle("-fx-background-color: #d7fffb;");
+        Label updateLabel = new Label("Mettre à jour la quantité d’un livre");
+        HBox updateInput = new HBox(10);
+        TextField updateIsbn = new TextField();
+        updateIsbn.setPromptText("ISBN");
+        TextField updateQuantite = new TextField();
+        updateQuantite.setPromptText("Nouvelle quantité");
+        Button updateBtn = new Button("Actualiser");
+        updateInput.getChildren().addAll(new Label("ISBN :"), updateIsbn, new Label("Quantité :"), updateQuantite, updateBtn);
+        updateStock.getChildren().addAll(updateLabel, updateInput);
 
-        Label addLivreLabel = new Label("Ajouter un livre");
+        // Vérifier disponibilité
+        VBox checkDispo = new VBox(5);
+        checkDispo.setPadding(new Insets(10));
+        checkDispo.setStyle("-fx-background-color: #d7fffb;");
+        Label checkLabel = new Label("Vérifier la disponibilité d’un livre");
+        HBox checkInput = new HBox(10);
+        TextField checkIsbn = new TextField();
+        checkIsbn.setPromptText("ISBN");
+        Button checkBtn = new Button("Vérifier");
+        checkInput.getChildren().addAll(new Label("ISBN :"), checkIsbn, checkBtn);
+        checkDispo.getChildren().addAll(checkLabel, checkInput);
 
-        HBox addLivreInputBox = new HBox(10);
+        // Passer une commande client
+        VBox commandeClient = new VBox(5);
+        commandeClient.setPadding(new Insets(10));
+        commandeClient.setStyle("-fx-background-color: #d7fffb;");
+        Label commandeLabel = new Label("Passer une commande pour un client");
+        HBox commandeInput = new HBox(10);
+        TextField commandeClientId = new TextField();
+        commandeClientId.setPromptText("ID Client");
+        TextField commandeIsbn = new TextField();
+        commandeIsbn.setPromptText("ISBN");
+        TextField commandeQuantite = new TextField();
+        commandeQuantite.setPromptText("Quantité");
+        Button commandeBtn = new Button("Commander");
+        commandeInput.getChildren().addAll(new Label("Client ID :"), commandeClientId, new Label("ISBN :"), commandeIsbn, new Label("Quantité :"), commandeQuantite, commandeBtn);
+        commandeClient.getChildren().addAll(commandeLabel, commandeInput);
 
-        Label isbnLabel = new Label("ISBN :");
-        TextField isbnArea = new TextField();
+        // Transfert depuis une autre librairie
+        VBox transfert = new VBox(5);
+        transfert.setPadding(new Insets(10));
+        transfert.setStyle("-fx-background-color: #d7fffb;");
+        Label transfertLabel = new Label("Transférer un livre depuis une autre librairie");
+        HBox transfertInput = new HBox(10);
+        TextField sourceLib = new TextField();
+        sourceLib.setPromptText("ID Librairie source");
+        TextField transfertIsbn = new TextField();
+        transfertIsbn.setPromptText("ISBN");
+        TextField transfertQuantite = new TextField();
+        transfertQuantite.setPromptText("Quantité");
+        Button transfertBtn = new Button("Transférer");
+        transfertInput.getChildren().addAll(new Label("Librairie source :"), sourceLib, new Label("ISBN :"), transfertIsbn, new Label("Quantité :"), transfertQuantite, transfertBtn);
+        transfert.getChildren().addAll(transfertLabel, transfertInput);
 
-        Label quantityLabel = new Label("Quantité :");
-        TextField quantityArea = new TextField();
+        // Ajouter tout au center
+        center.getChildren().addAll(addLivre, updateStock, checkDispo, commandeClient, transfert);
+        this.setCenter(center);
 
-        Button addButton = new Button("Ajouter");
-
-        addLivreInputBox.getChildren().addAll(isbnLabel, isbnArea, quantityLabel, quantityArea, addButton);
-        insideAddLivre.getChildren().addAll(addLivreLabel, addLivreInputBox);
-        addLivre.getChildren().add(insideAddLivre);
-
-
-
-        VBox updateQuantity = new VBox(5);
-        VBox insideUpdateQuantity = new VBox(3);
-        Label addUpdateQuantityLabel = new Label("Modifier quantité");
-        addLivre.getChildren().addAll(insideUpdateQuantity);
-        insideUpdateQuantity.getChildren().addAll(addUpdateQuantityLabel);
-
-
-        VBox checkAvailability = new VBox(5);
-
-        center.getChildren().addAll(addLivre, updateQuantity, checkAvailability);
-
-
-        // bottom of the borderPane
+        // BOTTOM
         HBox bottom = new HBox(10);
         Label credit = new Label("V1 par Jolan");
         bottom.getChildren().add(credit);
-
-        this.setCenter(center);
         this.setBottom(bottom);
-
     }
+
 
     public Vendeur getVendeur() {
         return this.vendeur;
