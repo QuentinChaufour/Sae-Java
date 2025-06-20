3b20d449fe0ad4d8aa519fb7a1dee87de560036c
Revan <quentin.chuafour@etu.univ-orleans.com>
2025-06-20 09:19:48 +0200

    exec

--- sae_java/exec.sh
+++ sae_java/exec.sh
@@ -1,10 +1,10 @@
-mvn compile
-mvn exec:java -Dexec.mainClass="com.sae_java.App"
+#mvn compile
+#mvn exec:java -Dexec.mainClass="com.sae_java.App"
 
 # pour générer la documentation dans le dossier target/apidoc
 # mvn javadoc:javadoc
 
 #pour lancer l'application
 
-#java -jar ./sae_java-1.0-SNAPSHOT.jar
+java -jar ./sae_java-2.0-SNAPSHOT.jar
 
